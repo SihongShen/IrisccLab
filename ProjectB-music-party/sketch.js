7f832dee0971c12603f19180512cc5c8bcef723f
SihongShen <ss18264@nyu.edu>
2023-11-29 20:52:23 +0800

    Project B updated

---
+++ ProjectB-music-party/sketch.js
@@ -0,0 +1,228 @@
+console.log("does this work?");
+
+let number = 4;
+let aSquare = 100;
+let squareSpacing = 25;
+let sumWidth = number*aSquare+(number-1)*squareSpacing;
+let sumHeight = sumWidth;
+let beginX = (600-sumWidth)/2;
+let beginY = (600-sumHeight)/2;
+
+let players=[];
+let fields=[];
+let counter=0;
+
+let firstInteraction=false;
+
+let r=260;
+let g=260;
+let b=260;
+
+let t=260;
+let tIncrease=-3;
+
+
+function preload(){
+
+  for(let i=0; i<number; i++){
+    let y=beginY+i*(aSquare+squareSpacing);
+    for(let j=0; j<number; j++){
+      let x=beginX+j*(aSquare+squareSpacing);
+
+      let path="track/pb"+(counter+1)+".mp3";
+      let sound = loadSound(path);
+
+      let field=new Field(x,y,sound, counter);
+      fields.push(field);
+
+      counter++
+    }
+  }
+
+  backgroundImage = loadImage("canvas-pictures/canvasBackground.jpg");
+
+}
+
+function setup(){
+  let cnv = createCanvas(600, 600);
+  cnv.parent("canvasWrapper");
+
+  players.push(new Player(50,50,87, 83, 65, 68));
+  players.push(new Player(550,550,38, 40, 37, 39));
+  players.push(new Player(550,50,73,75,74,76));
+  // console.log(fields)
+}
+
+function draw(){
+
+  if(firstInteraction == false){
+    image(backgroundImage,0,0,600,600);
+    background(r,g,b,20);
+
+    noStroke();
+    fill(4,0,250,85);
+    rect(0,0,width,height);
+
+    textFont("Black Ops One");
+    textSize(72);
+    noFill();
+    stroke(0, 250, 238);
+    strokeWeight(3);
+    text("Music Party", 72,210);
+
+    textFont("Gloria Hallelujah");
+    textSize(28);
+    noStroke();
+    fill(260,t);
+    text("Click anywhere to begin", 125, 390);
+
+    t += tIncrease;
+    if(t<=100 || t>=260){
+      tIncrease *=-1;
+    }
+  }else{
+    r=g=b=0;
+  background(r,g,b);
+  }
+  // console.log("track/pb"+counter+".mp3");
+
+  if(firstInteraction == false){
+    return
+  }
+
+
+  for(let i=0; i<fields.length; i++){
+    fields[i].display();
+    fields[i].checkPlayers()
+
+  }
+
+  for(let i=0; i<players.length; i++){
+    players[i].update();
+    players[i].display();
+  }
+  
+
+}
+
+class Field{
+  constructor(startX,startY,sound, name){
+    this.x = startX;
+    this.y = startY;
+    this.sound=sound;
+    this.vol = 0;
+    this.sound.setVolume(this.vol)
+    this.name = name;
+    this.onField = false;
+    
+  }
+
+  display(){
+    push()
+    translate(this.x, this.y);
+
+    stroke(260);
+    noFill();
+
+    rect(0,0,aSquare,aSquare,15);
+    text(this.name, 10, 10)
+
+    // console.log(this.sound)
+    if(this.vol == 1){
+      text("PLAYING", 10, 40);
+    }
+
+    pop();
+
+    // if(this.name == 1){
+
+    // }
+  }
+
+  checkPlayers() {
+    this.onField = false; 
+    for (let i = 0; i < players.length; i++) {
+      let players1X = players[i].x;
+      let players1Y = players[i].y;
+
+      if (
+        players1X > this.x &&
+        players1X < this.x + aSquare &&
+        players1Y > this.y &&
+        players1Y < this.y + aSquare
+      ) {
+        this.onField = true; 
+      }
+    }
+
+    if (this.onField) {
+      this.vol = 1;
+      this.sound.setVolume(this.vol); 
+    } else {
+      this.vol = 0;
+      this.sound.setVolume(this.vol); 
+    }
+  }
+}
+
+
+
+class Player{
+  constructor(startX,startY,up,down,left,right){
+    this.x = startX;
+    this.y = startY;
+    this.upKey = up;
+    this.downKey = down;
+    this.leftKey = left;
+    this.rightKey = right;
+
+    this.speed = 2;
+  }
+
+  update(){
+    // console.log(keyCode)
+    if(keyIsDown(this.upKey)){
+
+      this.y -=this.speed;
+    }
+    if(keyIsDown(this.downKey)){
+      this.y +=this.speed;
+    }
+    if(keyIsDown(this.leftKey)){
+      this.x -=this.speed;
+    }
+    if(keyIsDown(this.rightKey)){
+      this.x +=this.speed;
+    }
+
+  }
+
+  display(){
+    fill(255,3,3);
+    circle (this.x, this.y,20);
+
+  }
+
+
+
+}
+
+function mousePressed(){
+  if(firstInteraction == false){
+
+    for(i=0; i<fields.length; i++){
+      fields[i].sound.loop();
+    }
+    firstInteraction = true;
+  }
+
+  
+}
+
+
+function someoneClicked(event){
+  // console.log(event)
+  event.preventDefault();
+}
+
+window.addEventListener("keydown", someoneClicked);
--- ProjectBCanvas/sketch.js
+++ ProjectBCanvas/sketch.js
@@ -2,43 +2,131 @@ console.log("does this work?");
 
 let number = 4;
 let aSquare = 100;
-let squarePacing = 25;
-let sumWidth = number*aSquare+(number-1)*squarePacing;
+let squareSpacing = 25;
+let sumWidth = number*aSquare+(number-1)*squareSpacing;
 let sumHeight = sumWidth;
 let beginX = (600-sumWidth)/2;
 let beginY = (600-sumHeight)/2;
 
-let xSquare=[];
-let ySquare=[];
+let players=[];
+// let player1;
+let fields=[];
+let counter=0;
 
-let squareNumber;
+let firstInteraction=false;
 
-let player1;
-// let player2;
+function preload(){
+
+  for(let i=0; i<number; i++){
+    let y=beginY+i*(aSquare+squareSpacing);
+    for(let j=0; j<number; j++){
+      let x=beginX+j*(aSquare+squareSpacing);
+
+      let path="track/pb"+(counter+1)+".mp3";
+      let sound = loadSound(path);
+
+      let field=new Field(x,y,sound, counter);
+      fields.push(field);
+
+      counter++
+    }
+  }
+
+}
 
 function setup(){
   let cnv = createCanvas(600, 600);
   cnv.parent("canvasWrapper");
 
-  player1=new Player(50,50,87, 83, 65, 68);
-  // player2=new Player(550,550,73, 75, 74, 76);
-
-
-
+  players.push(new Player(50,50,87, 83, 65, 68));
+  players.push(new Player(550,550,38, 40, 37, 39));
+  // console.log(fields)
 }
 
 function draw(){
-    background(0);
+  background(0);
+  // console.log("track/pb"+counter+".mp3");
 
-    button();
+  if(firstInteraction == false){
+    return
+  }
 
-    player1.update();
-    player1.display();
 
-    // player2.update();
-    // player2.display();
+  for(let i=0; i<fields.length; i++){
+    fields[i].display();
+    fields[i].checkPlayers()
+
+  }
+
+  for(let i=0; i<players.length; i++){
+    players[i].update();
+    players[i].display();
+  }
+  
 
 }
+
+class Field{
+  constructor(startX,startY,sound, name){
+    this.x = startX;
+    this.y = startY;
+    this.sound=sound;
+    this.vol = 0;
+    this.sound.setVolume(this.vol)
+    this.name = name;
+    this.onField = false;
+    
+  }
+
+  display(){
+    push()
+    translate(this.x, this.y);
+
+    stroke(260);
+    noFill();
+
+    rect(0,0,aSquare,aSquare,15);
+    text(this.name, 10, 10)
+
+    // console.log(this.sound)
+    if(this.vol == 1){
+      text("PLAYING", 10, 40);
+    }
+
+    pop();
+
+    // if(this.name == 1){
+
+    // }
+  }
+
+  checkPlayers() {
+    this.onField = false; // 在检查之前将 onField 设置为 false，以便重新评估
+    for (let i = 0; i < players.length; i++) {
+      let players1X = players[i].x;
+      let players1Y = players[i].y;
+
+      if (
+        players1X > this.x &&
+        players1X < this.x + aSquare &&
+        players1Y > this.y &&
+        players1Y < this.y + aSquare
+      ) {
+        this.onField = true; // 如果任一玩家在区域内，将 onField 设置为 true
+      }
+    }
+
+    if (this.onField) {
+      this.vol = 1;
+      this.sound.setVolume(this.vol); // 如果有玩家在区域内，则设置音量为1
+    } else {
+      this.vol = 0;
+      this.sound.setVolume(this.vol); // 如果没有玩家在区域内，则设置音量为0
+    }
+  }
+}
+
+
 
 class Player{
   constructor(startX,startY,up,down,left,right){
@@ -53,8 +141,9 @@ class Player{
   }
 
   update(){
-
+    console.log(keyCode)
     if(keyIsDown(this.upKey)){
+
       this.y -=this.speed;
     }
     if(keyIsDown(this.downKey)){
@@ -66,52 +155,34 @@ class Player{
     if(keyIsDown(this.rightKey)){
       this.x +=this.speed;
     }
+
   }
 
   display(){
     fill(255,3,3);
     circle (this.x, this.y,20);
-  }
-
-  checkPosition(){
-    if(){
-      if(){
-
-      }
-      if(){
-
-      }
-      if(){
-
-      }
-      if(){
-
-      }
-    }
 
   }
+
+
 
 }
 
-function button(){
-  let radiusSquare = 15;
+function mousePressed(){
+  if(firstInteraction == false){
 
-  xSquare=[];
-  ySquare=[];
-
-  for (let i=0; i<number; i++){
-    let y=beginY+i*(aSquare+squarePacing);
-    for(let j=0; j<number; j++){
-      let x=beginX+j*(aSquare+squarePacing);
-      stroke(260);
-      noFill();
-      rect(x,y,aSquare,aSquare,radiusSquare);
-
-      xSquare.push (x);
-      ySquare.push (y);
-
+    for(i=0; i<fields.length; i++){
+      fields[i].sound.loop();
     }
+    firstInteraction = true;
   }
-
-  console.log(xSquare,ySquare);
+  
 }
+
+
+function someoneClicked(event){
+  // console.log(event)
+  event.preventDefault()
+}
+
+window.addEventListener("keydown", someoneClicked)
